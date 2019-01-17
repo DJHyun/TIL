adf68df9078f9161683c6724f86b435b3fa17ad1
DJHyun <djestiny4444@naver.com>
2019-01-17 17:52:27 +0900

    0117 third

---
+++ Algorithm/baekjoon/2908.상수.py
@@ -0,0 +1,6 @@
+# baekjoon source = "https://www.acmicpc.net/problem/2908"
+import sys
+numbers = list(sys.stdin.readline().rstrip().split())
+for i,n in enumerate(numbers):
+    numbers[i] = int(numbers[i][::-1])
+print(max(numbers))
