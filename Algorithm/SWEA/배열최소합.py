b3119921f6a4428cac637c6c0bb83c002329ff1d
DJHyun <djestiny4444@naver.com>
2019-02-21 22:30:03 +0900

    home 2019-02-21

--- Algorithm/SWEA/배열최소합.py
+++ Algorithm/SWEA/배열최소합.py
@@ -1,19 +1,20 @@
 import sys
 sys.stdin = open("배열최소합.txt", "r")
 
-def backtrack(a, k, input):
-    global maxcandi
-    c = [0] * maxcandi
+def backtrack(a, k, input,sum_num):
+    global sum_,N
+    c = [0] * N
 
     if k == input :
-        for i in range(1, k+1):
-            result.append(a[i])
+        if sum_>sum_num:
+            sum_=sum_num
     else:
-        k += 1
-        ncandi = construct(a, k, input, c)
-        for i in range(ncandi):
-            a[k] = c[i]
-            backtrack(a, k, input)
+        if sum_num < sum_:
+            k += 1
+            ncandi = construct(a, k, input, c)
+            for i in range(ncandi):
+                a[k] = c[i]
+                backtrack(a, k, input,sum_num+arr[a[k]][k-1])
 
 def construct(a, k, input, c):
     global ncandi
@@ -23,7 +24,6 @@ def construct(a, k, input, c):
         in_perm[a[i]] = True
     
     ncandi = 0
-
     for i in range(0, input):
         if in_perm[i] == False:
             c[ncandi] = i
@@ -34,18 +34,12 @@ def construct(a, k, input, c):
 T = int(input())
 for test_case in range(1, T + 1):
     N = int(input())
-    ncandi , maxcandi = N, N
+    ncandi = N
     a = [0] * (N+1)
-    result = [[] for x in range(N+1) ]
-    backtrack(a, 0, N)
-    sum_,vsum = 9999,0
-
+    sum_ = 9999
+    
     arr = [list(map(int, input().split())) for x in range(N)]
-    for i in arr:
-        print(i)
 
-    
-    print(result)
-    print(arr)
-    print(sum_)
-    
+    backtrack(a, 0, N, 0)
+
+    print(f'#{test_case} {sum_}')
