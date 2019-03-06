2a7a8b8df526f91c735bed8835e1d26e95ae61e3
DJHyun <djestiny4444@naver.com>
2019-03-07 01:36:40 +0900

    home 2019-03-07

--- Algorithm/baekjoon/2628_종이자르기.py
+++ Algorithm/baekjoon/2628_종이자르기.py
@@ -9,59 +9,51 @@ import sys
 '''
 y, x = map(int, sys.stdin.readline().split())
 cnt = int(sys.stdin.readline())
-arr = [[0] * (y + 1) for _ in range(x + 1)]
-x_len = [x]
-y_len = [y]
-sq = []
+arr = [[0] * (y) for _ in range(x)]
+max_,y_m = 0,0
+memo = []
 
 for i in range(cnt):
     dot, num = map(int, sys.stdin.readline().split())
-
-    print(dot, num)
-
-    if dot == 0:
-        if i == 0:
-            for j in range(i, len(x_len)):
-                x_len.append(x_len[j] - num)
-                y_len.append(y_len[j])
-                x_len.append(num)
-                y_len.append(y_len[j])
-        else:
-            for j in range(1 + (i - 1) * 2, len(x_len)):
-                x_len.append(x_len[j] - num)
-                y_len.append(y_len[j])
-                x_len.append(num)
-                y_len.append(y_len[j])
-    else:
-        if i == 0:
-            for j in range(i, len(y_len)):
-                y_len.append(y_len[j] - num)
-                x_len.append(x_len[j])
-                y_len.append(num)
-                x_len.append(x_len[j])
-        else:
-            for j in range(1 + (i - 1) * 2, len(y_len)):
-                y_len.append(y_len[j] - num)
-                x_len.append(x_len[j])
-                y_len.append(num)
-                x_len.append(x_len[j])
-
-    for j in range(len(x_len)):
-        print(x_len[j], y_len[j])
-
-    if dot == 0:
-        for j in range(y + 1):
-            if arr[num][j] == 1:
-                arr[num][j] += 1
-            else:
+    if (dot, num) not in memo:
+        if dot == 0:
+            arr.insert(num+1,[0]*len(arr[0]))
+            for j in range(len(arr[0])):
+                if arr[num][j] == 1:
+                    if num+1 <= len(arr)-1:
+                        arr[num+1][j] = 1
                 arr[num][j] = 1
-    else:
-        for j in range(x + 1):
-            if arr[j][num] == 1:
-                arr[j][num] += 1
-            else:
+        else:
+            for a in range(len(arr)):
+                arr[a].insert(num+1,0)
+            for j in range(len(arr)):
+                if arr[j][num] == 1:
+                    if num + 1 <= len(arr[0]) - 1:
+                        arr[j][num+1] = 1
                 arr[j][num] = 1
 
     for i in arr:
         print(i)
     print()
+
+    memo.append((dot,num))
+
+cou = 0
+for a in range(len(arr[0])):
+    if arr[0][a] == 0:
+        cou += 1
+    else:
+        max_ = max(max_,cou)
+        cou = 0
+max_ = max(max_, cou)
+
+cou = 0
+for a in range(len(arr)):
+    if arr[a][0] == 0:
+        cou += 1
+    else:
+        y_m = max(y_m,cou)
+        cou = 0
+y_m = max(y_m,cou)
+
+print(max_*y_m)
