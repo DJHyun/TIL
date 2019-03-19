b03370cba2867c1721640a1c2aa1779b239a8870
DJHyun <djestiny4444@naver.com>
2019-03-19 12:49:32 +0900

    2019-03-19

--- Algorithm/baekjoon/1003_피보나치함수.py
+++ Algorithm/baekjoon/1003_피보나치함수.py
@@ -3,13 +3,21 @@
 import sys
 
 def dp(n):
-    if n == 1 or n == 0:
+    global one, two
+    if n == 0:
+        one += 1
         return 1
-    
+
+    if n == 1:
+        two += 1
+        return 1
+
     if memo[n] != 0:
         return memo[n]
-    
-    memo[n] = dp(n-1) + dp(n-2)
+
+    print(n)
+    memo[n] = dp(n - 1) + dp(n - 2)
+    print('m', n, memo[n])
 
     return memo[n]
 
@@ -17,7 +25,9 @@ T = int(sys.stdin.readline())
 
 for test_case in range(T):
     num = int(sys.stdin.readline())
-    memo = [0]*num
-
+    memo = [0] * (num + 1)
+    one, two = 0, 0
     dp(num)
+
     print(memo)
+    print(f'#{test_case} {one} {two}')
