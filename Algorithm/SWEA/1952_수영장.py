fcfa98ec857a5724e731ff87b1efd576d8847587
DJHyun <djestiny4444@naver.com>
2019-04-03 21:47:36 +0900

    home 2019-04-03

--- Algorithm/SWEA/1952_수영장.py
+++ Algorithm/SWEA/1952_수영장.py
@@ -3,14 +3,15 @@ import sys
 sys.stdin = open("1952_수영장.txt", "r")
 
 T = int(input())
-for test_case in range(1, T + 1):
+for test_case in range(1, 2):
     prices = list(map(int, input().split()))
     days = list(map(int, input().split()))
     result = 0
-    r = [0] * 12
+    check = [0] * 12
     top = -1
 
     result = []
+
     for i in range(12):
         result.append([])
         for j in range(3):
@@ -18,33 +19,41 @@ for test_case in range(1, T + 1):
                 result[i].append([days[i], days[i] * prices[j], j])
             if j == 1:
                 result[i].append([days[i], prices[j], j])
-            if j == 2:
-                result[i].append([days[i], prices[j], j])
         top += 1
         result[i] = sorted(result[i], key=lambda x: x[1])
-        r[top] = result[i][0]
+        check[top] = result[i][0]
 
-    result = 0
-    for i in range(12):
-        if r[i][2] == -1 or r[i][0] == 0:
-            continue
+    result = 999999999
+    for num in range(12):
+        r = check[:]
+        score = 0
+        for i in range(12):
+            print((i+num)%12)
+            print()
+            if r[(i + num) % 12][2] == -1 or r[(i + num) % 12][0] == 0:
+                continue
+            if ((i + num) % 12) < 10:
+                if r[(i + num) % 12][1] + r[(i + num + 1) % 12][1] + r[(i + num + 2) % 12][1] > prices[2]:
+                    score += prices[2]
+                    r[(i + num + 1) % 12][2] = -1
+                    r[(i + num + 2) % 12][2] = -1
+                else:
+                    score += r[(i + num) % 12][1]
+            elif ((i + num) % 12) == 10:
+                if r[(i + num) % 12][1] + r[(i + num + 1) % 12][1] > prices[2]:
+                    score += prices[2]
+                    r[11][2] = -1
+                else:
+                    score += r[(i + num) % 12][1]
+            else:
+                if r[(i + num) % 12][1] > prices[2]:
+                    score += prices[2]
+                else:
+                    score += r[(i + num) % 12][1]
 
-        if r[i][1] + r[(i + 1) % 12][1] + r[(i + 2) ][1] > prices[2] or r[i % 12][2] == 2:
+            # if score >= result:
+            #     break
+            print(score)
+        result = min(score, result)
 
-            result += prices[2]
-
-            if i == 10 and r[0][2] != -1:
-                result -= r[0][1]
-            if i == 11 and r[0][2] != -1:
-                result -= r[0][1]
-            if i == 11 and r[1][2] != -1:
-                result -= r[1][1]
-
-            r[i % 12][2] = -1
-            r[(i + 1) % 12][2] = -1
-            r[(i + 2) % 12][2] = -1
-        else:
-            result += r[i][1]
-    print(r)
-
-    print(f'#{test_case} {min(prices[3],result)}')
+    print(f'#{test_case} {min(prices[3], result)}')
