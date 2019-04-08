f7e07896bfffb76603bcc49486642ca42d40f623
DJHyun <djestiny4444@naver.com>
2019-04-08 22:09:36 +0900

    2019-04-08

--- Algorithm/baekjoon/1722_순열의순서.py
+++ Algorithm/baekjoon/1722_순열의순서.py
@@ -2,39 +2,37 @@
 
 import sys
 
+def fato(x):
+    if x == 1:
+        return 1
+    return x * fato(x-1)
+
+
+
 def solution(flag, k, check):
     global cnt
 
-    if k == check:
-        cnt += 1
-        if a == m[1:]:
-            print(cnt)
-            return 0
 
-    if flag:
-        if cnt == m[1]:
-            print(' '.join(map(str,a)))
-            cnt += 1
-            return 0
 
-    for i in range(1, check + 1):
-        if not visited[i]:
-            visited[i] = 1
-            a.append(i)
-            fl = solution(flag, k + 1, check)
-            if fl == 0:
-                return 0
-            a.pop()
-            visited[i] = 0
+    if flag == 1:
+        for i in range(check):
+            a[check-i-1] = m[1]%(i+1)+1
+            m[1] //= (i+1)
+        print(a)
 
 n = int(sys.stdin.readline())
 m = list(map(int, sys.stdin.readline().split()))
-a = []
+a = [0]*n
 cnt = 0
 
+
+print(n)
+print(m)
 if m[0] == 1:
+    m[1] -= 1
     visited = [0] * (n + 1)
     solution(1, 0, n)
 else:
     visited = [0] * (n + 1)
     solution(0, 0, n)
+
