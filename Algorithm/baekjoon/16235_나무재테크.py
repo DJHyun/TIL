26a31c0f45ba210eff6d15e3912a0749ddb135ab
DJHyun <djestiny4444@naver.com>
2019-06-04 17:58:04 +0900

    2019-06-04

--- Algorithm/baekjoon/16235_나무재테크.py
+++ Algorithm/baekjoon/16235_나무재테크.py
@@ -44,100 +44,70 @@ def check(x, y):
     return True
 
 n, m, k = map(int, input().split())
-arr = [[5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] * n for _ in range(n)]
+arr = [[[5] for _ in range(n)] * n for _ in range(n)]
+for i in range(n):
+    for j in range(n):
+        arr[i][j] += [0] * 200
 add_arr = [list(map(int, input().split())) for _ in range(n)]
-tree = [[0] * 3 for _ in range(m)]
-print(arr)
-for i in arr:
-    print(i)
-print()
+result = 0
+
 for i in range(m):
     tree_input = list(map(int, input().split()))
     tree_input[0] -= 1
     tree_input[1] -= 1
-    print(tree_input[2])
-    print(arr[tree_input[0]][tree_input[1]][tree_input[2]])
     arr[tree_input[0]][tree_input[1]][tree_input[2]] += 1
-# tree = [list(map(int, input().split())) for _ in range(m)]
-tree = sorted(tree, key=lambda x: x[2])
+    result += 1
+
 dx, dy = [-1, -1, -1, 0, 0, 1, 1, 1], [-1, 0, 1, -1, 1, -1, 0, 1]
 
-print(n, m, k)
+idx = 0
+while idx < k:
+    # 봄
+    for i in range(n):
+        for j in range(n):
 
-for i in add_arr:
-    print(i)
-print()
+            flag = True
+            tree = [0] * 201
+            # 봄
+            for z in range(1, 201):
+                for a in range(arr[i][j][z]):
+                    if flag:
+                        if arr[i][j][0] >= z:
+                            arr[i][j][0] -= z
+                            arr[i][j][z] -= 1
+                            tree[z + 1] += 1
+                        else:
+                            flag = False
+                            arr[i][j][0] += z // 2
+                            arr[i][j][z] -= 1
+                            result -= 1
+                    else:
+                        tree[0] += z // 2
+                        arr[i][j][z] -= 1
+                        result -= 1
+            # 여름
+            for z in range(201):
+                arr[i][j][z] += tree[z]
 
-for i in tree:
-    print(i)
-print()
-#
-# idx = 0
-# while idx < k:
-#     # 봄
-#     for i in range(m):
-#         if not tree[i][3]:
-#             if arr[tree[i][0]][tree[i][1]] >= tree[i][2]:
-#                 arr[tree[i][0]][tree[i][1]] -= tree[i][2]
-#                 tree[i][2] += 1
-#             else:
-#                 tree[i][3] = 1
-#
-#     # for i in arr:
-#     #     print(i)
-#     # print()
-#     # for i in tree:
-#     #     print(i)
-#     # print()
-#
-#     # 여름
-#     for i in range(m):
-#         if tree[i][3]:
-#             arr[tree[i][0]][tree[i][1]] += tree[i][2] // 2
-#
-#     # for i in arr:
-#     #     print(i)
-#     # print()
-#
-#     # 가을
-#     new_tree = 0
-#     for mix in range(m):
-#         mix += new_tree
-#         if not tree[mix][3] and not tree[mix][2] % 5:
-#             for j in range(8):
-#                 tx = tree[mix][0] + dx[j]
-#                 ty = tree[mix][1] + dy[j]
-#                 if check(tx, ty):
-#                     tree.insert(0,([tx, ty, 1, 0]))
-#                     mix += 1
-#                     new_tree += 1
-#                     m += 1
-#
-#
-#     # for i in arr:
-#     #     print(i)
-#     # print()
-#     # for i in tree:
-#     #     print(i)
-#     # print()
-#
-#     # 겨울
-#     for i in range(n):
-#         for j in range(n):
-#             arr[i][j] += add_arr[i][j]
-#
-#     # for i in arr:
-#     #     print(i)
-#     # print()
-#     # for i in tree:
-#     #     print(i)
-#
-#     idx += 1
-#
-#     print(m,idx)
-#
-# result = 0
-# for i in range(m):
-#     if not tree[i][3]:
-#         result += 1
-# print(result)
+    for i in range(n):
+        for j in range(n):
+
+            # 가을
+            for z in range(1, 201):
+                if not z % 5:
+                    for a in range(arr[i][j][z]):
+                        for b in range(8):
+                            tx = i + dx[b]
+                            ty = j + dy[b]
+                            if check(tx, ty):
+                                arr[tx][ty][1] += 1
+                                result += 1
+
+            # 겨울
+            arr[i][j][0] += add_arr[i][j]
+
+    idx += 1
+    for i in arr:
+        print(i)
+    print()
+print(result)
--- Algorithm/baekjoon/1722_순열의순서.py
+++ Algorithm/baekjoon/1722_순열의순서.py
@@ -1,14 +1,32 @@
 # baekjoon source = "https://www.acmicpc.net/problem/1722"
 import sys
-import itertools
+
+def solution(k,number):
+    global n, cnt
+
+
+    if k == n:
+        print(number)
+        cnt += 1
+        if m[0] == 1:
+            if cnt == m[1]:
+                print(' '.join(number))
+                return True
+        else:
+            if number == str(m[1])+str(m[2])+str(m[3])+str(m[4]):
+                print(cnt)
+                return True
+
+    for i in range(1, n+1):
+        if not visited[i]:
+            visited[i] = 1
+            if solution(k+1, number+str(i)):
+                break
+            visited[i] = 0
 
 n = int(sys.stdin.readline())
 m = list(map(int, sys.stdin.readline().split()))
+visited = [0] * (n + 1)
+cnt = 0
 
-number = list(itertools.permutations(range(1,n+1)))
-if m[0] == 1:
-    print(' '.join(list(map(str,number[m[1]-1]))))
-else:
-    print(number.index(tuple(m[1:]))+1)
-
-
+solution(0,'')
