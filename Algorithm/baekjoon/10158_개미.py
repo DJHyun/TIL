98174118241677f3b20d849e6d7446114cce4f7d
DJHyun <djestiny4444@naver.com>
2019-06-10 17:55:58 +0900

    2019-06-10

--- Algorithm/baekjoon/10158_개미.py
+++ Algorithm/baekjoon/10158_개미.py
@@ -6,42 +6,54 @@
 
 0 1
 
+6 4
+4 1
+7
+
+0 1
+
+
 40000 40000
 0 0
 200000000
 '''
 
-y, x = map(int, input().split())
+h, w = map(int, input().split())
 q, p = map(int, input().split())
 t = int(input())
 
-# p_flag, q_flag = 1, 1
-# for i in range(t % (w * h)):
-#     if p + 1 > h:
-#         p_flag = 0
-#     elif p - 1 < 0:
-#         p_flag = 1
-#
-#     if p_flag:
-#         p += 1
-#     else:
-#         p -= 1
-#
-#     if q + 1 > w:
-#         q_flag = 0
-#     elif q - 1 < 0:
-#         q_flag = 1
-#
-#     if q_flag:
-#         q += 1
-#     else:
-#         q -= 1
-#
-# print(p, q)
-
-print(x, y)
+print(w, h)
 print(p, q)
-print(t)
 
-print(t % (x * 2))
-print(t % (y * 2))
+xt = t % (2 * w)
+xt, xd = xt % w, xt // w
+
+xy = t % (2 * h)
+xy, yd = xy % h, xy // h
+
+print("asdfasdfasdf", xt, xd)
+print('asdf', xy, yd)
+p_flag, q_flag = 1, 1
+for i in range(xt):
+    if p + 1 > w:
+        p_flag = 0
+    elif p - 1 < 0:
+        p_flag = 1
+
+    if p_flag:
+        p += 1
+    else:
+        p -= 1
+
+for i in range(xy):
+    if q + 1 > h:
+        q_flag = 0
+    elif q - 1 < 0:
+        q_flag = 1
+
+    if q_flag:
+        q += 1
+    else:
+        q -= 1
+
+print(p, q)
