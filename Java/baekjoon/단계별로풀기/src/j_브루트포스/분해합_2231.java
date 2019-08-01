18fb4fd1b93be5621462099e21080515567d3b7e
DJHyun <djestiny4444@naver.com>
2019-08-01 17:54:54 +0900

    2019-08-01

---
+++ Java/baekjoon/단계별로풀기/src/j_브루트포스/분해합_2231.java
@@ -0,0 +1,15 @@
+//baekjoon source = "https://www.acmicpc.net/problem/2557"
+package j_브루트포스;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class 분해합_2231 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+}
