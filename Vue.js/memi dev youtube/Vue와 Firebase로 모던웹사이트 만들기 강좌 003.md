c10f2589e04981720cac906e9c1251841caa9cda
DJHyun <djestiny4444@naver.com>
2019-10-23 17:52:32 +0900

    2019-10-23

---
+++ Vue.js/memi dev youtube/Vue와 Firebase로 모던웹사이트 만들기 강좌 003.md
@@ -0,0 +1,10 @@
+# Vue와 Firebase로 모던 웹사이트 만들기 강좌 003
+
+##### 1. 배포하기
+
+- Hosting
+- dist폴더에다 Hosting 만들기
+- 배포하기 전에 build하기
+- package.json
+  - firebase:deploy라는 scripts로 build와 deploy를 한 번에 하기
+
--- Vue.js/memi dev youtube/project/test/src/views/Home.vue
+++ Vue.js/memi dev youtube/project/test/src/views/Home.vue
@@ -1,17 +1,3 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div>abcdef</div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
